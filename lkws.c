29ed0ce3c3744c8a7ad2d4f086cb0b4a0420a288
robdelacruz <robdelacruz@gmail.com>
2023-05-12 00:51:37 +0800

    add help page

--- lkws.c
+++ lkws.c
@@ -20,7 +20,9 @@
 
 void handle_sigint(int sig);
 void handle_sigchld(int sig);
-void parse_args(int argc, char *argv[], LKConfig *cfg);
+int parse_args(int argc, char *argv[], LKConfig *cfg);
+void print_help();
+void print_sample_config();
 
 LKHttpServer *httpserver;
 
@@ -40,16 +42,25 @@ LKHttpServer *httpserver;
 // lkws /var/www/testsite/ --cgidir=cgi-bin
 // lkws -f sites.conf
 int main(int argc, char *argv[]) {
+    int z;
     signal(SIGPIPE, SIG_IGN);           // Don't abort on SIGPIPE
     signal(SIGINT, handle_sigint);      // exit on CTRL-C
     signal(SIGCHLD, handle_sigchld);
 
+
     lk_alloc_init();
     LKConfig *cfg = lk_config_new();
-    parse_args(argc, argv, cfg);
+    z = parse_args(argc, argv, cfg);
+    if (z == 1) {
+        lk_config_free(cfg);
+        print_help();
+        exit(0);
+    }
+
+    printf("Little Kitten Web Server version 0.9 (%s -h for instructions)\n", argv[0]);
     httpserver = lk_httpserver_new(cfg);
 
-    int z = lk_httpserver_serve(httpserver);
+    z = lk_httpserver_serve(httpserver);
     if (z == -1) {
         return z;
     }
@@ -75,6 +86,69 @@ void handle_sigchld(int sig) {
     errno = tmp_errno;
 }
 
+void print_help() {
+    printf(
+"Usage:\n"
+"lkws [homedir] [port] [host] [-f configfile] [--cgidir=cgifolder]\n"
+"\n"
+"configfile = configuration file containing site settings\n"
+"             see sample configuration file below\n"
+"homedir    = absolute or relative path to a home directory\n"
+"             defaults to current working directory if not specified\n"
+"port       = port number to bind to server\n"
+"             defaults to 8000\n"
+"host       = IP address to bind to server\n"
+"             defaults to localhost\n"
+"Examples:\n"
+"lkws ./testsite/ 8080\n"
+"lkws /var/www/testsite/ 8080 127.0.0.1\n"
+"lkws /var/www/testsite/ --cgidir=cgi-bin\n"
+"lkws -f sites.conf\n"
+"\n"
+"Source code and docs at https://github.com/robdelacruz/lkwebserver\n"
+"\n"
+    );
+}
+
+void print_sample_config() {
+    printf(
+"Sample config file:\n"
+"\n"
+"serverhost=127.0.0.1\n"
+"port=5000\n"
+"\n"
+"# Matches all other hostnames\n"
+"hostname *\n"
+"homedir=/var/www/testsite\n"
+"alias latest=latest.html\n"
+"\n"
+"# Matches http://localhost\n"
+"hostname localhost\n"
+"homedir=/var/www/testsite\n"
+"\n"
+"# http://littlekitten.xyz\n"
+"hostname littlekitten.xyz\n"
+"homedir=/var/www/testsite\n"
+"cgidir=cgi-bin\n"
+"alias latest=latest.html\n"
+"alias about=about.html\n"
+"alias guestbook=cgi-bin/guestbook.pl\n"
+"alias blog=cgi-bin/blog.pl\n"
+"\n"
+"# http://newsboard.littlekitten.xyz\n"
+"hostname newsboard.littlekitten.xyz\n"
+"proxyhost=localhost:8001\n"
+"\n"
+"# Format description:\n"
+"# The host and port number is defined first, followed by one or more\n"
+"# host config sections. The host config section always starts with the\n"
+"# 'hostname <domain>' line followed by the settings for that hostname.\n"
+"# The section ends on either EOF or when a new 'hostname <domain>' line\n"
+"# is read, indicating the start of the next host config section.\n"
+"\n"
+    );
+}
+
 typedef enum {PA_NONE, PA_FILE} ParseArgsState;
 
 // lkws [homedir] [port] [host] [--cgidir=<cgidir>] [-f <configfile>]
@@ -93,7 +167,7 @@ typedef enum {PA_NONE, PA_FILE} ParseArgsState;
 // lkws /var/www/testsite/ 8080 127.0.0.1
 // lkws /var/www/testsite/ --cgidir=guestbook
 // lkws -f littlekitten.conf
-void parse_args(int argc, char *argv[], LKConfig *cfg) {
+int parse_args(int argc, char *argv[], LKConfig *cfg) {
     ParseArgsState state = PA_NONE;
     int is_homedir_set = 0;
     int is_port_set = 0;
@@ -104,6 +178,10 @@ void parse_args(int argc, char *argv[], LKConfig *cfg) {
         if (state == PA_NONE && !strcmp(arg, "-f")) {
             state = PA_FILE;
             continue;
+        }
+        if (state == PA_NONE &&
+            (!strcmp(arg, "-h") || !strcmp(arg, "--help"))) {
+            return 1;
         }
         // --cgidir=cgifolder
         if (state == PA_NONE && !strncmp(arg, "--cgidir=", 9)) {
@@ -137,5 +215,6 @@ void parse_args(int argc, char *argv[], LKConfig *cfg) {
             is_host_set = 1;
         }
     }
+    return 0;
 }
 
